eb80910522d07f9822a1eb8ecf6916321aaca32f
Adam <296786475@qq.com>
2022-02-22 10:56:29 +0800

    meeting-rooms-ii VIP

---
+++ adam_20220215_leetcode/src/高频题/_11_盛最多水的容器.java
@@ -0,0 +1,16 @@
+package 高频题;
+
+/** https://leetcode-cn.com/problems/container-with-most-water/
+ * @auther adam
+ * @date 2022/2/22
+ * @apiNote 高频题
+ */
+public class _11_盛最多水的容器 {
+    public int maxArea(int[] height) {
+        if (height == null || height.length == 0) {
+            return 0;
+        }
+
+        return 0;
+    }
+}
